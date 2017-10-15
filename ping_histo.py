8d1260b8a992662d76410943fc0ddb07e0ec9406
Philipp Klaus <philipp.klaus@gmail.com>
2017-10-15 09:30:06 +0200

    move code from main() into its own ping() function

--- ping_histo.py
+++ ping_histo.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import subprocess
-import re
-import sys
+import subprocess, re, sys
 
 single_matcher = re.compile("(?P<bytes>\d+) bytes from (?P<IP>\d+.\d+.\d+.\d+): icmp_seq=(?P<sequence>\d+) ttl=(?P<ttl>\d+) time=(?P<time>\d+(.\d+)?) ms")
 # should match lines like this:
@@ -14,6 +12,33 @@ end_matcher = re.compile("rtt min/avg/max/mdev = (?P<min>\d+.\d+)/(?P<avg>\d+.\d
 # alternative matcher for a different ping utility:
 #end_matcher = re.compile("round-trip min/avg/max/stddev = (\d+.\d+)/(\d+.\d+)/(\d+.\d+)/(\d+.\d+)")
 
+def ping(host=None, count=4, interval=1.0, debug=False):
+    ping = subprocess.Popen(
+        ["ping", "-c", str(count), "-i", str(interval), host],
+        stdout = subprocess.PIPE,
+        stderr = subprocess.STDOUT
+    )
+
+    times = []
+    sentinel = b"" if sys.version_info[0] >= 3 else ""
+    for line in iter(ping.stdout.readline, sentinel):
+        line = line.decode('ascii').strip()
+        if not line: continue
+        if debug: print("Analyzing this line: " + line)
+        match = single_matcher.match(line)
+        if match:
+            if debug: print("Matches found: {}".format(match.groups()))
+            time = float(match.group('time'))
+            times.append(time)
+            print(time)
+            sys.stdout.flush()
+            continue
+        match = end_matcher.match(line)
+        if match:
+            if debug: print("Matches found: {}".format(match.groups()))
+            continue
+        if debug: print("Didn't understand this line: " + line)
+
 def main():
     import argparse
     parser = argparse.ArgumentParser(description='Ping a host and create histogram.')
@@ -23,37 +48,14 @@ def main():
     parser.add_argument('--debug', '-d', action='store_true', help='Enable debug output for this script')
     args = parser.parse_args()
 
-    ping = subprocess.Popen(
-        ["ping", "-c", str(args.count), "-i", str(args.interval), args.host],
-        stdout = subprocess.PIPE,
-        stderr = subprocess.STDOUT
-    )
-
     try:
-        times = []
-        sentinel = b"" if sys.version_info[0] >= 3 else ""
-        for line in iter(ping.stdout.readline, sentinel):
-            line = line.decode('ascii').strip()
-            if not line: continue
-            if args.debug: print("Analyzing this line: " + line)
-            match = single_matcher.match(line)
-            if match:
-                if args.debug: print("Matches found: {}".format(match.groups()))
-                time = float(match.group('time'))
-                times.append(time)
-                print(time)
-                continue
-            match = end_matcher.match(line)
-            if match:
-                if args.debug: print("Matches found: {}".format(match.groups()))
-                continue
-            if args.debug: print("Didn't understand this line: " + line)
+        ping(host=args.host, count=args.count, interval=args.interval, debug=args.debug)
     except KeyboardInterrupt:
         sys.exit()
 
     if args.debug: print("Exit Code of the ping command: " + str(ping.returncode))
+
     sys.exit(ping.returncode)
 
 if __name__ == "__main__":
     main()
-
